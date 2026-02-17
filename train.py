62531933f18ceb5064b8e8f3d0034bba077cd1b1
Eric Krämer <kraemer.eric@live.de>
2026-02-17 02:21:23 +0100

    Moved dataloader to seperate file

--- train.py
+++ train.py
@@ -2,50 +2,19 @@ import math
 import sys
 import time
 from contextlib import nullcontext
-from operator import itemgetter
-from pathlib import Path
 from typing import Literal
 
-import numpy as np
 import torch
-from torch import Tensor
-
-import wandb
 from omegaconf import OmegaConf
-
+from torch import Tensor
 from wandb.plot import CustomChart
 
+import wandb
 from nanoGPT import util
 from nanoGPT.causal_self_attention import CausalSelfAttention
+from nanoGPT.data_loader import DataLoader
 from nanoGPT.gpt import GPT
 from nanoGPT.gpt_config import GPTConfig
-
-class DataLoader:
-    def __init__(self, cfg: GPTConfig, device: str):
-        data_path: Path = Path(cfg.data.path) / cfg.data.dataset
-        self._train_dir: Path = data_path / 'train.bin'
-        self._val_dir: Path = data_path / 'val.bin'
-        self._batch_size: int = cfg.data.batch_size
-        self._block_size: int = cfg.data.block_size
-        self._device: str = device
-        self._cuda: bool = device.startswith('cuda')
-
-    def get_batch(self, split: str):
-        # We recreate np.memmap every batch to avoid a memory leak, as per
-        # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
-        if split == 'train':
-            data = np.memmap(self._train_dir, dtype=np.uint16, mode='r')
-        else:
-            data = np.memmap(self._val_dir, dtype=np.uint16, mode='r')
-        ix = torch.randint(len(data) - self._block_size, (self._batch_size,))
-        x = torch.stack([torch.from_numpy((data[i:i+self._block_size]).astype(np.int64)) for i in ix])
-        y = torch.stack([torch.from_numpy((data[i+1:i+1+self._block_size]).astype(np.int64)) for i in ix])
-        if self._cuda:
-            # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
-            x, y = x.pin_memory().to(self._device, non_blocking=True), y.pin_memory().to(self._device, non_blocking=True)
-        else:
-            x, y = x.to(self._device), y.to(self._device)
-        return x, y
 
 
 class TrainEvalHandler:
